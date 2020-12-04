39997eb9599a62a55fac145243e35768754e39d0
Tracey Saada <tracey.saada@outlook.com>
2020-12-04 18:48:39 +0000

    changed to ajax not var in js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple",
-  "method": "GET",
-  "headers": {
-      "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
-      "x-rapidapi-key": "dbe3f6aea3mshcb668f8d0f325d8p1c4d2ajsnb5fd0a60e96d"
-  }
-}
-$.ajax(settings).done(function (response) {
-  console.log(response);
+
+$.ajax({
+type: "GET",
+  url: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple",
+  dataType: "json",
+
+
+})
 
 //input code ticket 4
 
@@ -20,17 +16,18 @@ $(document).ready(function () {
     $(".btn").on("click", function () {
       var searchMovie = $("#myInput").val();
       console.log(searchMovie);
-function searchMovie
+
+    
+
       $.ajax({
         type: "GET",
-        url: "http://www.omdbapi.com/?i=tt3896198&apikey=" + apikey + searchMovie 
+        url: "http://www.omdbapi.com/?i=tt3896198&apikey=",
         dataType: "json",
-
-
 
         //input code ticket 5
 
       }
     //final ticket to complete program
-     
-    })
+      )}
+
+    )})
