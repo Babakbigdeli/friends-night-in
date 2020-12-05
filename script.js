826d88836006f75dceacdad7ef67bb01233c90f7
Tracey Saada <tracey.saada@outlook.com>
2020-12-05 11:55:06 +0000

    added first search function

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
 
-$.ajax({
-type: "GET",
-  url: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple",
-  dataType: "json",
+//search recipe search button
+$(document).ready(function(){
+  $("#recipeBtn").on("click", function(){
+    var recipeSearch = $("#recipeInput").val();
+    recipeData(recipeSearch);
 
+  });
 
 })
-
-//input code ticket 4
-
-
-var apiKey = "e7dd88c3";
-
-      $.ajax({
-        type: "GET",
-        url: "http://www.omdbapi.com/?i=tt3896198&" + apiKey + searchMovie 
-        dataType: "json",
-
-        $(document).ready(function () { 
-          $(".btn").on("click", function () {
-            var searchMovie = $("#myInput").val();
-            console.log(searchMovie);
-
-
-        //input code ticket 5
-
-      }
-    //final ticket to complete program
-      )}
-
-    )})
