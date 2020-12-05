fa58b34bb8c14a69ef446dbb2436032bc5c21d78
Babakbigdeli <babakbigdeli@gmail.com>
2020-12-05 14:11:06 +0000

    verify api

--- script.js
+++ script.js
@@ -8,11 +8,8 @@ $(document).ready(function(){
   });
 function recipeData(recipeSearch){
   $.ajax({
-    url: "https://api.edamam.com/search/q=" + recipeSearch ,
-    type: "GET",
-    xhrFields: {
-      withCredentials: true
-   }
+    url: "https://api.edamam.com/search?app_id=40897fdb&app_key=e7085ffc3bbf333e4fcc1dfd79fa54fd&q=" + recipeSearch,
+    method: "GET",
   }).then(function(response){
     console.log("I am the ", response);
   });
