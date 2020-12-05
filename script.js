5602f2291ae4f3c317d74dfb0cbbfd1655beffd5
omayma ahmad <omayma.ahmad25@gmail.com>
2020-12-05 14:25:20 +0000

    added movie button function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     recipeData(recipeSearch);
 
   });
+  // recipe button funciton/ API call
 function recipeData(recipeSearch){
   $.ajax({
     url: "https://api.edamam.com/search?app_id=40897fdb&app_key=e7085ffc3bbf333e4fcc1dfd79fa54fd&q=" + recipeSearch,
@@ -14,4 +15,23 @@ function recipeData(recipeSearch){
     console.log("I am the ", response);
   });
 }
+
+//search movie button 
+$("#movieBtn").on("click", function (){
+  var movieSearch = $("#movieInput").val(); 
+  movieData(movieSearch);
+
+});
+
+// movie button function/ API call
+function movieData(movieSearch) {
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=e7dd88c3" + movieSearch,
+    method: "GET",
+  }).then(function (response){
+    console.log("I am the ", response);
+  });
+}
+
+
 })
