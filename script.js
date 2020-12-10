422a35e4b3488fa600f8fe7027c24c0fbb281633
Babakbigdeli <babakbigdeli@gmail.com>
2020-12-10 21:47:44 +0000

    added more styling to html

--- script.js
+++ script.js
@@ -68,11 +68,10 @@ $(document).ready(function () {
       var iconHtml = $("<img>").attr("src", imageUrl);
       var recipeLink = $("<a>").attr("href", randomRecipe.shareAs).attr("target", "_blank").text("Read your recipe at " + randomRecipe.source);
       var label = $("<p>").addClass("card-text").text(randomRecipe.label);
-      var recipeUrl = $("<p>").addClass("card-text").text(randomRecipe.shareAs);
       var source = $("<p>").addClass("card-text").text(randomRecipe.source);
 
       image.append(iconHtml)
-      $(".recipe-card").append(image, recipeLink, label, recipeUrl, source);
+      $(".recipe-card").append(image, recipeLink, label, source);
 
       $(".level-left").append(card);
     });
